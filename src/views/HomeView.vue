33219dd5cb7c37867857c7c215fef7d5a7e8e9ca
Dimanda <traperwaze@gmail.com>
2024-01-03 11:14:49 +0700

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import bt from 'bluetooth-terminal'
+let terminal = new bt()
+
+function connectClick() {
+  terminal.connect().then(() => {
+    alert(terminal.getDeviceName())
+  })
+}
+</script>
+
+<template>
+  <main>
+    <button class="btn" @click="connectClick">Connect</button>
+  </main>
+</template>
