2f59cde417f58671834c7f9ebaba53818a29eee7
Dimanda <traperwaze@gmail.com>
2024-05-25 13:51:25 +0700

    refine layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,9 @@ const deviceName = ref('')
 const log = ref('')
 const logEl = ref(null)
 const consoleInput = ref('')
+const showTerminal = ref('')
+
+const connState = ref('idle')
 
 const writeLog = (data) => {
     log.value += "\n" + data
@@ -24,16 +27,17 @@ const connectClick = () => {
         alert(m)
     }).catch(() => { })
 
+    connState.value = 'connecting'
     terminal.connect()
         .then((t) => {
             deviceName.value = terminal.getDeviceName() ?
                 terminal.getDeviceName() : deviceName.value;
-            terminal.send("help").then((d) => { alert(d) })
-                .catch((e) => alert(e))
+            connState.value = 'connected'
         })
         .catch((e) => {
-            console.log(e);
+            connState.value = 'idle'
             alert(e)
+            writeLog(e)
         })
 }
 
@@ -47,13 +51,14 @@ const consoleInputEnter = () => {
 </script>
 
 <template>
-    <div class="container max-w-md mx-auto h-full relative flex flex-col">
-        <NavBar />
+    <div class="container max-w-md mx-auto h-full flex flex-col">
+        <NavBar @click="showTerminal = !showTerminal" />
         <main class="my-4 px-2 md:px-0 flex-grow">
-            <button class="btn" @click="connectClick">Connect</button>
-            <span>{{ deviceName }}</span>
+            <button class="btn btn-sm me-2" @click="connectClick">Connect</button>
+            <span v-if="connState == 'connecting'" class="loading loading-dots loading-md"></span>
+            <span v-if="connState == 'connected'">Connected to: <b>{{ deviceName }}</b></span>
         </main>
-        <div class="absolute bottom-0 bg-base-300 w-full px-4 py-0 font-mono border-t border-t-current">
+        <div v-if="showTerminal" class="bg-base-300 w-full px-4 py-0 font-mono border-t border-t-current">
             <pre ref="logEl" class="text-sm max-h-20 h-20 overflow-y-auto overflow-x-hidden whitespace-pre-wrap">
                 {{ log }}
             </pre>
